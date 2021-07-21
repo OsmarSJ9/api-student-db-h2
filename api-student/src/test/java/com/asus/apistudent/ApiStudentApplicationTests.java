74766b666221529a536c565b612bf09aba383cd1
OsmarSJ9 <osmarsj9@gmail.com>
2021-07-21 17:06:25 -0400

    First commit

---
+++ api-student/src/test/java/com/asus/apistudent/ApiStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asus.apistudent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
