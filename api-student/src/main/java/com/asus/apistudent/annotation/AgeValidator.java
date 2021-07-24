4dd0830806cedc5bf3b7d8f6a1431ad600856ce6
OsmarSJ9 <osmarsj9@gmail.com>
2021-07-23 20:38:32 -0400

    Correction number 2

--- api-student/src/main/java/com/asus/apistudent/annotation/AgeValidator.java
+++ api-student/src/main/java/com/asus/apistudent/annotation/AgeValidator.java
@@ -1,31 +1,27 @@
 package com.asus.apistudent.annotation;
 
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 
-public class AgeValidator implements ConstraintValidator<AgeValidation, Integer>{
-	@Min(value = 1, message = "User age must be greater than 0")
-	@Max(value = 100, message = "User age must be smaller than 100")
-    protected long minAge;
+public class AgeValidator implements ConstraintValidator<AgeValidation, Integer> {
 
-    @Override
-    public void initialize(AgeValidation ageValue) {
-        this.minAge = ageValue.value();
-    }
-	
+	protected long minAge;
+
+	@Override
+	public void initialize(AgeValidation ageValue) {
+		this.minAge = ageValue.value();
+	}
+
 	@Override
 	public boolean isValid(Integer value, ConstraintValidatorContext context) {
 
-        if(value > 0 && value < 100) {
-        	return true;
-        }
-        	return false;
+		if (value > 0 && value < 100) {
+			return true;
+		}
+		return false;
 
 	}
-
-
 
 }
